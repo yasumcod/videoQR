f5a5f2ed90433f683652b886c2c7927b3ebf5506
門田康宏 <mondenyasuhiro@kadotayasuhiro.local>
2024-07-15 18:08:06 +0900

    mod

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import React, { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
@@ -17,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Hello React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
